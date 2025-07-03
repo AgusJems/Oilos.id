ba6335433bceb3d56042620de176f82e30b30a4a
AgusJems <59831192+AgusJems@users.noreply.github.com>
2025-07-03 22:47:39 +0700

    Pemesanan UI

--- src/pages/OrderLIst/Pemesanan.tsx
+++ src/pages/OrderLIst/Pemesanan.tsx
@@ -2,17 +2,12 @@ import PageBreadcrumb from "../../components/common/PageBreadCrumb";
 import ComponentCard from "../../components/common/ComponentCard";
 import PageMeta from "../../components/common/PageMeta";
 import PemesananTable from "../../components/orderlist/PemesananTable";
-import { useState } from "react";
-import ModalPemesanan from "../../components/orderlist/ModalPemesanan";
-
+import { useModal } from "../../hooks/useModal";
+import { Modal } from "../../components/ui/modal";
+import Button from "../../components/ui/button/Button";
 
 export default function Pemesanan() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const handleSubmit = () => {
-        console.log("Data disimpan!");
-        setIsModalOpen(false);
-    };
+  const { isOpen, openModal, closeModal } = useModal();
   return (
     <>
       <PageMeta
@@ -20,18 +15,82 @@ export default function Pemesanan() {
         description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
       <PageBreadcrumb pageTitle="Pemesanan" />
-      <div className="space-y-6">
-        <button onClick={() => setIsModalOpen(true)} className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
-            Buka Modal
-        </button>
+      
+      <div className="flex justify-end mb-3">
+        <Button onClick={openModal} className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
+          Tambah Data
+        </Button>
 
-        <ModalPemesanan
-            isOpen={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-            onSubmit={handleSubmit}
-        />
+        <Modal isOpen={isOpen} onClose={closeModal} className="max-w-[700px] p-6 lg:p-10">
+          <div className="flex flex-col px-2 overflow-y-auto custom-scrollbar">
+            <div>
+              <h5 className="mb-2 font-semibold text-gray-800 modal-title text-theme-xl dark:text-white/90 lg:text-2xl">
+                dfsdfdsf
+              </h5>
+              <p className="text-sm text-gray-500 dark:text-gray-400">
+                Plan your next big moment: schedule or edit an event to stay on
+                track
+              </p>
+            </div>
+            <div className="mt-8">
+              <div>
+                <div>
+                  <label className="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                    Event Title
+                  </label>
+                  <input
+                    id="event-title"
+                    type="text"
+                    className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+                  />
+                </div>
+              </div>
+              <div className="mt-6">
+                <label className="block mb-4 text-sm font-medium text-gray-700 dark:text-gray-400">
+                  Event Color
+                </label>
+              </div>
+
+              <div className="mt-6">
+                <label className="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                  Enter Start Date
+                </label>
+                <div className="relative">
+                  <input
+                    id="event-start-date"
+                    type="date"
+                    className="dark:bg-dark-900 h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 pl-4 pr-11 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+                  />
+                </div>
+              </div>
+
+              <div className="mt-6">
+                <label className="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                  Enter End Date
+                </label>
+                <div className="relative">
+                  <input
+                    id="event-end-date"
+                    type="date"
+                    className="dark:bg-dark-900 h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 pl-4 pr-11 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-3 mt-6 modal-footer sm:justify-end">
+              <button onClick={closeModal} type="button" className="flex w-full justify-center rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] sm:w-auto">
+                Close
+              </button>
+              <button onClick={closeModal} type="button" className="btn btn-success btn-update-event flex w-full justify-center rounded-lg bg-brand-500 px-4 py-2.5 text-sm font-medium text-white hover:bg-brand-600 sm:w-auto">
+                Simpan
+              </button>
+            </div>
+          </div>
+        </Modal>
+      </div>
+      <div className="space-y-6">
         <ComponentCard title="Tabel Pemesanan">
-            <PemesananTable/>
+          <PemesananTable />
         </ComponentCard>
       </div>
     </>
