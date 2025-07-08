610023b8569dd8594bed66c9781a6dae277f8302
AgusJems <59831192+AgusJems@users.noreply.github.com>
2025-07-08 15:18:54 +0700

    profile landing

--- src/landing/Navbar/Navbar.tsx
+++ src/landing/Navbar/Navbar.tsx
@@ -1,20 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { HiMenuAlt1, HiMenuAlt3 } from "react-icons/hi";
 import DarkMode from "./DarkMode";
 import ResponsiveMenu from "./ResponsiveMenu";
 import { NavLinks } from "../Navbar/NavLinks";
 import { Link } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
+interface DecodedUser {
+  Name?: string;
+  CodeRefferal?: string;
+}
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
   const [openSubmenu, setOpenSubmenu] = useState<number | null>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => setShowMenu(!showMenu);
   const toggleSubmenu = (id: number) => {
     setOpenSubmenu(openSubmenu === id ? null : id);
   };
-
   const hasLinks = NavLinks.length > 0;
+
+  const [user, setUser] = useState<DecodedUser | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const decoded: DecodedUser = jwtDecode(token);
+        setUser(decoded);
+      } catch (err) {
+        console.error("Token invalid:", err);
+        setUser(null);
+      }
+    } else {
+      setUser(null);
+    }
+
+    function handleClickOutside(event: MouseEvent) {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+        }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+    };
+
+  }, []);
+
+const toggleDropdown = () => setIsOpen((prev) => !prev);
+
+const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/landing";
+};
 
   return (
     <nav className="sticky top-0 z-50 w-full backdrop-blur bg-white/80 dark:bg-black/80 shadow-md dark:text-white duration-300">
@@ -23,18 +66,18 @@ const Navbar: React.FC = () => {
                 {/* Logo */}
                 <Link to="/landing">
                     <img
-                    src="/images/logo/logo.png"
-                    alt="Logo"
-                    width={100}
-                    height={70}
-                    className="dark:hidden"
+                        src="/images/logo/logo.png"
+                        alt="Logo"
+                        width={100}
+                        height={70}
+                        className="dark:hidden"
                     />
                     <img
-                    className="hidden dark:block"
-                    src="./images/logo/logo-white.png"
-                    alt="Logo"
-                    width={100}
-                    height={70}
+                        className="hidden dark:block"
+                        src="./images/logo/logo-white.png"
+                        alt="Logo"
+                        width={100}
+                        height={70}
                     />
                 </Link>
 
@@ -77,13 +120,62 @@ const Navbar: React.FC = () => {
                     )}
                 </div>
                 
+                
                 <div className="hidden md:flex gap-3">
-                    <a href="/signin" className="cursor-pointer bg-gradient-to-r from-green-500 to-green-400 hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
-                        Sign In
-                    </a>
-                    <a href="/signup" className="cursor-pointer bg-gradient-to-r from-green-500 to-green-400 hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
-                        Daftar Member
-                    </a>
+                    {!user ? (
+                        <div className="hidden md:flex gap-3">
+                            <a href="/signin" className="cursor-pointer bg-gradient-to-r from-green-500 to-green-400 hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                                Sign In
+                            </a>
+                            <a href="/signup" className="cursor-pointer bg-gradient-to-r from-green-500 to-green-400 hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                                Daftar Member
+                            </a>
+                        </div>
+                    ) : (
+                        <div className="flex flex-col items-center w-full gap-6 xl:flex-row-reverse relative" ref={dropdownRef}>
+                            {/* Avatar */}
+                            <div onClick={toggleDropdown}
+                                className="flex items-center justify-center w-14 h-14 text-2xl font-bold text-green-800 bg-green-100 rounded-full cursor-pointer">
+                                {user.Name?.charAt(0).toUpperCase() || "U"}
+                            </div>
+
+                            {/* Dropdown Logout */}
+                            {isOpen && (
+                                <div className="absolute right-0 top-19 z-50 w-48 bg-white border border-gray-200 rounded-xl shadow-lg p-2 dark:bg-gray-800 dark:border-gray-700">
+                                    <button
+                                        onClick={handleLogout}
+                                        className="flex items-center gap-2 w-full px-3 py-2 text-sm font-medium ttext-gray-700 rounded-lg group hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300">
+                                        <svg
+                                            className="w-5 h-5"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            strokeWidth="2"
+                                            viewBox="0 0 24 24"
+                                        >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M17 16l4-4m0 0l-4-4m4 4H7"
+                                        />
+                                        </svg>
+                                        Sign out
+                                    </button>
+                                </div>
+                            )}
+
+                            {/* Nama & Kode */}
+                            <div className="order-3 xl:order-2">
+                                <h4 className="mb-1 text-lg font-semibold text-center text-gray-800 dark:text-white/90 xl:text-left">
+                                {user.Name || "User"}
+                                </h4>
+                                <div className="flex flex-col items-center gap-1 text-center xl:flex-row xl:gap-3 xl:text-left">
+                                <p className="text-sm text-gray-500 dark:text-gray-400">
+                                    {user.CodeRefferal || "-"}
+                                </p>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
 
                 {/* Menu Mobile */}
