a9f422d244402f5927564afd2aeee9057e44af18
Abdul Qodir Ibrahim <bahim007@gmail.com>
2025-07-13 23:42:41 +0700

    feat: integrate Tiptap editor for news description input

--- src/components/admin-page/berita/BeritaTable.tsx
+++ src/components/admin-page/berita/BeritaTable.tsx
@@ -12,6 +12,7 @@ import { Modal } from "../../ui/modal";
 import Input from "../../form/input/InputField";
 import Label from "../../form/Label";
 import FileInput from "../../form/input/FileInput";
+import TiptapEditor from "../../form/input/TiptapEditor";
 
 interface NewsItem {
   id: number;
@@ -194,12 +195,7 @@ export default function BeritaTable() {
         </div>
         <div className="mb-3">
           <Label>Deskripsi</Label>
-          <Input
-            type="text"
-            placeholder="Deskripsi"
-            value={formData.description}
-            onChange={(e) => setFormData({ ...formData, description: e.target.value })}
-          />
+          <TiptapEditor></TiptapEditor>
         </div>
         <div className="mb-3">
           <Label>Upload Gambar</Label>
