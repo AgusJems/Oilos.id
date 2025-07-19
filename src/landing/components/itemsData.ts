6edab159da96e311862ab06d80f0b36672c8eef1
AgusJems <59831192+AgusJems@users.noreply.github.com>
2025-07-20 00:13:01 +0700

    update itemList admin

---
+++ src/landing/components/itemsData.ts
@@ -0,0 +1,38 @@
+export interface ListBarangItem {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  price:number;
+}
+
+export const fetchActiveNews = async (): Promise<ListBarangItem[]> => {
+  try {
+    const res = await fetch("http://localhost:3001/api/getActiveItems");
+    const result = await res.json();
+
+    if (!res.ok) {
+      throw new Error(result.message || "Gagal mengambil data barang");
+    }
+
+    return result.data;
+  } catch (err) {
+    console.error("Error fetching active news:", err);
+    return [];
+  }
+};
+
+export const fetchListBarangById = async (id: number): Promise<ListBarangItem | null> => {
+  try {
+    const res = await fetch(`http://localhost:3001/api/getItemsById/${id}`);
+    const result = await res.json();
+
+    if (!res.ok) throw new Error(result.message || "Gagal mengambil detail barang");
+
+    return result.data;
+  } catch (err) {
+    console.error("Error fetching detail news:", err);
+    return null;
+  }
+};
+
